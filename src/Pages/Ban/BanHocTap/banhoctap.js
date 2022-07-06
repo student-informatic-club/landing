46b53a08f4e5b9c283361d42bf5490ec6b731bb5
Lunatic <96194845+VuMinhQuanDev@users.noreply.github.com>
2022-07-06 17:37:02 +0700

    fix position close btn

--- src/Pages/Ban/BanHocTap/banhoctap.js
+++ src/Pages/Ban/BanHocTap/banhoctap.js
@@ -1,9 +1,10 @@
 const BanHocTap = {
-    title: 'Ban',
-    name: "Học Tập",
-    registerMes: 'Bấm đăng kí để có cơ hội trở thành thành viên của ban học tập nhé',
-    clb: 'student informatic club',
-    btn: 'Đăng kí CTV',
-}
+  title: "Ban",
+  name: "Học Tập",
+  registerMes:
+    "Bấm đăng kí để có cơ hội trở thành thành viên của ban học tập nhé",
+  clb: "student informatic club",
+  btn: "Đăng kí CTV",
+};
 
-export default BanHocTap
+export default BanHocTap;

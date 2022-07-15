5cfdb54d6908a92daefdafbde7d39770b0f7998c
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2022-07-16 05:39:26 +0700

    add loading animataion, scroll top func and fix render bugs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useLocation, Switch, useHistory } from "react-router-dom";
 import AppRoute from "./utils/AppRoute";
 import ScrollReveal from "./utils/ScrollReveal";
 import ReactGA from "react-ga";
-import ScrollToTop from "./hooks/ScroolToTop";
+import ScrollToTop from "./utils/ScroolToTop";
+import Loading from "./utils/Loading";
 
 // Layouts
 import LayoutDefault from "./layouts/LayoutDefault";
@@ -54,44 +55,46 @@ const App = () => {
       <ScrollReveal
         ref={childRef}
         children={() => (
-          <ScrollToTop>
-            <Switch>
-              <AppRoute
-                exact
-                path="/"
-                component={Home}
-                layout={LayoutDefault}
-              />
-              <AppRoute
-                exact
-                path="/home"
-                component={Home}
-                layout={LayoutDefault}
-              />
-              <AppRoute exact path="/event" component={Event} />
-              <AppRoute exact path="/event/:postID" component={EventDetail} />
-              <AppRoute exact path="/blog" component={Blog} />
-              <AppRoute exact path="/blog/:postID" component={BlogDetail} />
-              <AppRoute exact path="/ban-hoc-tap" component={BanHocTap} />
-              <AppRoute exact path="/ban-ky-thuat" component={BanKyThuat} />
-              <AppRoute exact path="/ban-ho-tro" component={BanHoTro} />
-              <AppRoute
-                exact
-                path="/ban-truyen-thong"
-                component={BanTruyenThong}
-              />
-              {history.location.pathname === "/admin" &&
-                (loginStatus === false || loginStatus === null) &&
-                history.push("/loginAdmin")}
-              <AppRoute
-                exact
-                path="/admin"
-                component={AdminPage}
-                layout={LayoutAdmin}
-              />
-              (<AppRoute exact path="/loginAdmin" component={login} />)
-            </Switch>
-          </ScrollToTop>
+          <Loading>
+            <ScrollToTop>
+              <Switch>
+                <AppRoute
+                  exact
+                  path="/"
+                  component={Home}
+                  layout={LayoutDefault}
+                />
+                <AppRoute
+                  exact
+                  path="/home"
+                  component={Home}
+                  layout={LayoutDefault}
+                />
+                <AppRoute exact path="/event" component={Event} />
+                <AppRoute exact path="/event/:postID" component={EventDetail} />
+                <AppRoute exact path="/blog" component={Blog} />
+                <AppRoute exact path="/blog/:postID" component={BlogDetail} />
+                <AppRoute exact path="/ban-hoc-tap" component={BanHocTap} />
+                <AppRoute exact path="/ban-ky-thuat" component={BanKyThuat} />
+                <AppRoute exact path="/ban-ho-tro" component={BanHoTro} />
+                <AppRoute
+                  exact
+                  path="/ban-truyen-thong"
+                  component={BanTruyenThong}
+                />
+                {history.location.pathname === "/admin" &&
+                  (loginStatus === false || loginStatus === null) &&
+                  history.push("/loginAdmin")}
+                <AppRoute
+                  exact
+                  path="/admin"
+                  component={AdminPage}
+                  layout={LayoutAdmin}
+                />
+                (<AppRoute exact path="/loginAdmin" component={login} />)
+              </Switch>
+            </ScrollToTop>
+          </Loading>
         )}
       />
       {/* <NotificationContainer /> */}

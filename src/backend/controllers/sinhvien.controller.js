0eee25e12fba62459fb4996f6ddc467d82f71d9e
minhdev-ui <dominh162002@gmail.com>
2022-07-17 15:06:41 +0700

    fix bug

--- src/backend/controllers/sinhvien.controller.js
+++ src/backend/controllers/sinhvien.controller.js
@@ -2,15 +2,15 @@ import axios from "axios";
 
 
 export async function getSinhVien() {
-    return axios.get('http://localhost:4000/sinhvien')
+    return axios.get('http://localhost:3000/sinhvien')
 }
 
 export function addNewSinhVien(obj) {
-    axios.post('http://localhost:4000/sinhvien/add', obj)
+    axios.post('http://localhost:3000/sinhvien/add', obj)
     .then(res => console.log(res.data));
 }
 
 export function updateSinhVien(id, obj) {
-    axios.post(`http://localhost:4000/sinhvien/update/${id}`, obj)
+    axios.post(`http://localhost:3000/sinhvien/update/${id}`, obj)
     .then(res => console.log(res.data))
 }

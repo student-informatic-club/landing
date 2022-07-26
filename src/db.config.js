f69009820ed955cfc323bd4a943da91df5573d9e
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2022-07-26 22:43:16 +0700

    add

--- src/db.config.js
+++ src/db.config.js
@@ -2,13 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyDeZbcf3bBdO3iATWnorfnEPKCPjTRocZI",
-    authDomain: "sic-db-53c2a.firebaseapp.com",
-    projectId: "sic-db-53c2a",
-    storageBucket: "sic-db-53c2a.appspot.com",
-    messagingSenderId: "122514351199",
-    appId: "1:122514351199:web:42d59db75f77756376a7ea",
-    measurementId: "G-CKLD3S22GJ"
+  // apiKey: "AIzaSyDeZbcf3bBdO3iATWnorfnEPKCPjTRocZI",
+  // authDomain: "sic-db-53c2a.firebaseapp.com",
+  // projectId: "sic-db-53c2a",
+  // storageBucket: "sic-db-53c2a.appspot.com",
+  // messagingSenderId: "122514351199",
+  // appId: "1:122514351199:web:42d59db75f77756376a7ea",
+  // measurementId: "G-CKLD3S22GJ"
+  apiKey: "AIzaSyA9ZZ_5oBbigQyC1iZvT6uXGgj93M3MpfE",
+  authDomain: "learn-fire-base-efb71.firebaseapp.com",
+  projectId: "learn-fire-base-efb71",
+  storageBucket: "learn-fire-base-efb71.appspot.com",
+  messagingSenderId: "126236039493",
+  appId: "1:126236039493:web:06b545c79fea6466c9062c",
 };
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);

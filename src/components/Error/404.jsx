32a71d1ddc0ef8b97eeb1f20d5a53422b96336cb
minhdev-ui <dominh162002@gmail.com>
2022-09-07 17:07:34 +0700

    remove admin

---
+++ src/components/Error/404.jsx
@@ -0,0 +1,13 @@
+const Error404 = () => {
+    return (
+        <div className="error-wrapper">
+            <div>
+                <img src="" alt="" />
+                <h1>404 Error</h1>
+            </div>
+            <h2>Page Not Found</h2>
+        </div>
+    )
+}
+
+export default Error404
